ef268a7fbc27262cf3882fd869804b9e272a1259
Alessio <alexdenik955@gmail.com>
2026-01-29 01:15:06 +0100

    first commit

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'nutri-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js'
+      ])
+    )
+  );
+});
